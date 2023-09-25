20fe79a3317652ca98f55fb97f739aae3c231f30
vitalikkk <vitalikgermak6@gmail.com>
2023-09-25 14:21:21 +0300

    add jsx method create element in react

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-let title = React.createElement(
-    'h1',
-    { id: 'titel', className: 'littel-title' },
-    'Hello world'
+// let title = React.createElement(
+//     'h1',
+//     { id: 'titel', className: 'littel-title' },
+//     'Hello world'
+// )
+let title = (
+    <div className="block">
+        <h1 className="title">Hello World</h1>
+    </div>
+)
+const list = (
+    <ul>
+        <li>List item 1</li>
+        <li>List item 2</li>
+        <li>List item 3</li>
+    </ul>
+)
+let res = (
+    <div className="wrapper">
+        {title} {list}
+    </div>
 )
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{title}</React.StrictMode>)
+root.render(<React.StrictMode>{res}</React.StrictMode>)
