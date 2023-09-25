a653979405f33229d36c2e92dc113c6c580f8201
vitalikkk <vitalikgermak6@gmail.com>
2023-09-25 14:02:11 +0300

    create first element in react

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+let title = React.createElement(
+    'h1',
+    { id: 'titel', className: 'littel-title' },
+    'Hello world'
+)
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode></React.StrictMode>)
+root.render(<React.StrictMode>{title}</React.StrictMode>)
