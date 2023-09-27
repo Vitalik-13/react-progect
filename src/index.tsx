8f8a627fd85c97ea7ae9e2b608db78959599d358
vitalikkk <vitalikgermak6@gmail.com>
2023-09-27 12:05:21 +0300

    add functional react component

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-// let title = React.createElement(
-//     'h1',
-//     { id: 'titel', className: 'littel-title' },
-//     'Hello world'
-// )
-let title = (
-    <div className="block">
-        <h1 className="title">Hello World</h1>
-    </div>
-)
-const list = (
-    <ul>
-        <li>List item 1</li>
-        <li>List item 2</li>
-        <li>List item 3</li>
-    </ul>
-)
-let res = (
-    <div className="wrapper">
-        {title} {list}
-    </div>
-)
+function App() {
+    return <h1>Hello App component</h1>
+}
+const Two = () => {
+    return <h2>Hello world</h2>
+}
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{res}</React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <App />
+        <Two />
+    </React.StrictMode>
+)
