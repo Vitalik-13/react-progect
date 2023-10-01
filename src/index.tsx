f75d0ffb97a582f51a4bd85a5f6279898c8944bd
vitalikkk <vitalikgermak6@gmail.com>
2023-10-01 12:13:58 +0300

    remove prctick files

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Titel from './Titel'
-import Content from './content'
 
-const App = () => {
-    return (
-        <>
-            <Titel title="React" year={2020} />
-            <Titel title="App" year={1502} />
-            <Content
-                content1="Привіт Петро як твої справи?"
-                content2="Привіт мої справи чудово, а як твої?"
-            />
-        </>
-    )
-}
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-)
+root.render(<React.StrictMode></React.StrictMode>)
