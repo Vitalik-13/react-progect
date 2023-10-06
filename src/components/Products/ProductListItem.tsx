d6a507eb2a72c4c30bd2f2b8b9978ccfa747d27a
vitalikkk <vitalikgermak6@gmail.com>
2023-10-06 13:38:25 +0300

    add three cardListitem and add their props

--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -1,16 +1,32 @@
 import { Button, Card, CardActions, CardContent } from '@mui/material'
 import './ProductListItem.scss'
-type Props = {}
-const ProductListItem = (props: Props) => {
+type Props = {
+    productTitle: string
+    productsDscription: string
+    productType: string
+    productsFeatures: string
+    productsPrice: number
+}
+const ProductListItem = ({
+    productTitle,
+    productsDscription,
+    productType,
+    productsFeatures,
+    productsPrice,
+}: Props) => {
     return (
         <Card variant="outlined" className="product">
             <CardContent>
-                <h4 className="product-title">Iphone 14 Pro</h4>
-                <p className="product-description">This is Iphone 14 Pro</p>
-                <div className="product-featuers">Type: Phone</div>
-                <div className="product-featuers">Capacity: 256Gb</div>
+                <h4 className="product-title">{productTitle}</h4>
+                <p className="product-description">
+                    This is Iphone {productsDscription}pro
+                </p>
+                <div className="product-featuers">Type:{productType}</div>
+                <div className="product-featuers">
+                    Capacity:{productsFeatures}Gb
+                </div>
                 <div className="product-price">
-                    Price: <strong>$1200</strong>
+                    Price: <strong>${productsPrice}</strong>
                 </div>
                 <CardActions className="buttons-wrapper">
                     <Button variant="outlined">Add to cart</Button>
--- src/components/Products/ProductsList.tsx
+++ src/components/Products/ProductsList.tsx
@@ -17,7 +17,31 @@ const ProductsList = (props: Props) => {
             </Typography>
             <Grid container spacing={4}>
                 <Grid item xs={12} sm={4}>
-                    <ProductListItem />
+                    <ProductListItem
+                        productTitle="Iphone 14 Pro"
+                        productsDscription="14"
+                        productType=" Phone"
+                        productsFeatures="132"
+                        productsPrice={1200}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    <ProductListItem
+                        productTitle="Iphone 13 Pro"
+                        productsDscription="13"
+                        productType="Phone"
+                        productsFeatures="64"
+                        productsPrice={900}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    <ProductListItem
+                        productTitle="Iphone 15 Pro"
+                        productsDscription="15"
+                        productType="Phone"
+                        productsFeatures="512"
+                        productsPrice={1500}
+                    />
                 </Grid>
             </Grid>
         </>
