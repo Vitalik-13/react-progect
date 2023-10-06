eda4c45eaef84c1eab5a03965a22f576197e9aca
vitalikkk <vitalikgermak6@gmail.com>
2023-10-06 14:13:35 +0300

    add productsArray

---
+++ src/utils/ProductsArray.ts
@@ -0,0 +1,52 @@
+type Product = {
+    productTitle: string
+    productsDscription: string
+    productType: string
+    productsFeatures: string
+    productsPrice: number
+}
+const ProductsArray: Product[] = [
+    {
+        productTitle: 'Iphone 13 Pro',
+        productsDscription: '13',
+        productType: 'Phone',
+        productsFeatures: '64',
+        productsPrice: 900,
+    },
+    {
+        productTitle: 'Iphone 14 Pro',
+        productsDscription: '14',
+        productType: 'Phone',
+        productsFeatures: '128',
+        productsPrice: 1000,
+    },
+    {
+        productTitle: 'Iphone 15 Pro',
+        productsDscription: '15',
+        productType: 'Phone',
+        productsFeatures: '215',
+        productsPrice: 1500,
+    },
+    {
+        productTitle: 'Iphone 13 Pro Max',
+        productsDscription: '13 ',
+        productType: 'Phone',
+        productsFeatures: '215',
+        productsPrice: 1200,
+    },
+    {
+        productTitle: 'Iphone 11',
+        productsDscription: '11',
+        productType: 'Phone',
+        productsFeatures: '32',
+        productsPrice: 500,
+    },
+    {
+        productTitle: 'Iphone X ',
+        productsDscription: 'X',
+        productType: 'Phone',
+        productsFeatures: '64',
+        productsPrice: 400,
+    },
+]
+export default ProductsArray
