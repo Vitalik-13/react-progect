efc549be111f9dc2ae104e955e6bb436e34c4276
vitalikkk <vitalikgermak6@gmail.com>
2023-10-08 14:33:48 +0300

    add block product-qantiti

--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -1,4 +1,10 @@
-import { Button, Card, CardActions, CardContent } from '@mui/material'
+import {
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    TextField,
+} from '@mui/material'
 import './ProductListItem.scss'
 
 type Props = {
@@ -37,6 +43,11 @@ const ProductListItem = ({
                 <CardActions className="buttons-wrapper">
                     <Button variant="outlined">Add to cart</Button>
                 </CardActions>
+                <div className="product-qantiti">
+                    <Button variant="outlined">+</Button>
+                    <TextField size="small" value={1} />
+                    <Button variant="outlined">-</Button>
+                </div>
             </CardContent>
         </Card>
     )
