eef116ccc73d6d41b2693216b6780c48ab33940a
vitalikkk <vitalikgermak6@gmail.com>
2023-11-02 15:20:10 +0200

    add object.assing in prevState

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -16,10 +16,18 @@ const App = (props: Props) => {
         1: 5,
         2: 5,
     })
+    const addToProductCard = (id: number, count: number) => {
+        setProductInCard((prevstate) =>
+            Object.assign({}, prevstate, {
+                [id]: prevstate[id] + count,
+            })
+        )
+    }
     return (
         <StyledEngineProvider injectFirst>
             <CssBaseline />
             <Header productIncard={productIncard} />
+            <button onClick={() => addToProductCard(2, 5)}>add to card</button>
             <Main addTocard={addTocard} />
             <Footer />
         </StyledEngineProvider>
